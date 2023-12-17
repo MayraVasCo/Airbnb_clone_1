40bd2d0c031f3ee68ac545978fcb04dfffff5725
MayraVasCo <mayravas2808@gmail.com>
2023-12-17 15:09:12 -0800

    new changes

--- console.py
+++ console.py
@@ -1,19 +1,29 @@
 import cmd
 
 class HBNBCommand(cmd.Cmd):
+    """
+    Clase que define un intérprete de comandos para una aplicación hbnb.
+    """
+
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
-        """Exit the program"""
+        """
+        Salir del programa.
+        """
         return True
 
     def do_EOF(self, arg):
-        """Exit the program"""
+        """
+        Salir del programa al llegar al final del archivo (Ctrl+D).
+        """
         return True
 
     def do_help(self, arg):
-        """Show help"""
-        # Personaliza la implementación del comando help si es necesario
+        """
+        Mostrar la ayuda.
+        """
+        # Personalizar la implementación del comando help si es necesario
         super().do_help(arg)
 
 if __name__ == "__main__":
